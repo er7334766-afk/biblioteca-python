460c8ed569f36599e3c7dfe8164f960405ec3eda
MrWiny <167271581+Ed928923482@users.noreply.github.com>
2026-04-13 16:29:40 -0600

    arreglo deploy render

--- biblioteca.py
+++ biblioteca.py
@@ -581,4 +581,8 @@ def inject_user():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+    )
